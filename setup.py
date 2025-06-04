73f697eb9fcca0dcc7c9bb8526e91db6b7d2eff6
TestingRabbit-pixel <TestingRabbit-pixel@users.noreply.github.com>
2025-06-04 00:59:45 +0000

    Update setup.py to explicitly include src directory

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='prometheus_test',
     version='0.1.0',
-    packages=find_packages(include=['prometheus_test', 'src']),
+    packages=find_packages(include=['src', 'prometheus_test']),
     install_requires=[
         'pytest',
     ],
