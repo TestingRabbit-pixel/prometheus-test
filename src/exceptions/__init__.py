5b866715e372856c721eef001b07406eea04e8de
TestingRabbit-pixel <TestingRabbit-pixel@users.noreply.github.com>
2025-06-04 00:16:12 +0000

    Initialize exceptions package

---
+++ src/exceptions/__init__.py
@@ -0,0 +1,6 @@
+"""
+Custom exception hierarchy for CoinGecko API errors.
+
+This module defines a comprehensive set of exceptions to handle 
+various error scenarios when interacting with the CoinGecko API.
+"""
